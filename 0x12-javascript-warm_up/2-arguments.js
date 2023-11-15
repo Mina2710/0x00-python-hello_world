06596fe6cb12f4012c979c0712a3d7d4017e0182
Mina2710 <125763571+Mina2710@users.noreply.github.com>
2023-11-16 01:57:43 +0200

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
+// @count: Holds the count of arguments passed when executing the script
 const count = process.argv.length;
 console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
