30c6de7a0d0f7ab78c4feb1d2f8dd86c5c0eb51e
Mina2710 <125763571+Mina2710@users.noreply.github.com>
2023-11-30 12:38:08 +0200

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
 # Check if the user provided a URL argument
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
-
 # Get the URL from the argument
 URL=$1
-
 # Send a GET request using curl and display the size of the response body in bytes
 curl -s -o /dev/null -w "%{size_download}\n" "$URL"
